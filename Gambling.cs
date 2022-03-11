6fb9f8957c9f91d4a87c82d501561245a201aeba
Anuj Kumar <96485818+anujk01@users.noreply.github.com>
2022-03-11 11:25:47 +0530

    Add files via upload

---
+++ Gambling.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssignmentGambling
+{
+    public class Gambling
+    {
+        public void Gamblerstake()
+        {
+            const int bet = 100;
+            const int everygame = 1;
+            int result;
+        }
+
+        public int WinLoose()
+        {
+            int result = new Random().Next(0, 2);
+            return result;
+        }
+
+        public int Resign()
+        {
+            int win = 50;
+
+            if (win > 50) ;
+            {
+                Console.WriteLine("Gamer Resign" + win);
+                return win;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AssignmentGambling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Gambling obj = new Gambling();
+            //obj.Gamblerstake();
+
+            //Gambling obj = new Gambling();
+            //obj.WinLoose();
+
+            Gambling obj = new Gambling();
+            obj.Resign();
+        }
+    }
+}
